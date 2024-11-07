dea2296de6c1b6acd4242b3b88b3d7289aa783c7
Stan Lukuvka <stanluku@gmail.co.nz>
2024-11-07 16:10:39 +1300

    fix: set print format

--- frappe/printing/page/print/print.js
+++ frappe/printing/page/print/print.js
@@ -737,7 +737,7 @@ frappe.ui.form.PrintView = class {
 		this.print_sel
 			.empty()
 			.add_options([
-				this.get_default_option_for_select(__("Select Print Format")),
+				// this.get_default_option_for_select(__("Select Print Format")),
 				...this.print_formats,
 			]);
 		return (
