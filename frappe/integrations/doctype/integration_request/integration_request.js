c8164728c8cc5c3b824959f6b0c790435c7f75be
Tom Fretwell <Tom@Fxmed.co.nz>
2024-12-03 15:54:52 +1300

    Fix: Formatting

--- frappe/integrations/doctype/integration_request/integration_request.js
+++ frappe/integrations/doctype/integration_request/integration_request.js
@@ -8,24 +8,22 @@ frappe.ui.form.on("Integration Request", {
 				const prettyJSON = JSON.stringify(JSON.parse(frm.doc.data), null, 4);
 				frm.fields_dict.data.$wrapper.html(`<pre style="white-space: pre-wrap; word-wrap: break-word;">${prettyJSON}</pre>`);
 			} catch {
-				
+
 			}
 
 			frm.add_custom_button(__('Copy Payload'), function () {
-				if (frm.doc.data) {
-					navigator.clipboard.writeText(frm.doc.data).then(() => {
-						frappe.show_alert({
-							message: __('Payload copied to clipboard!'),
-							indicator: 'green',
-						});
-					}).catch((err) => {
-						frappe.msgprint({
-							title: __('Error'),
-							message: __('Failed to copy data to clipboard: ') + err,
-							indicator: 'red',
-						});
+				navigator.clipboard.writeText(frm.doc.data).then(() => {
+					frappe.show_alert({
+						message: __('Payload copied!'),
+						indicator: 'green',
 					});
-				}
+				}).catch((err) => {
+					frappe.msgprint({
+						title: __('Error'),
+						message: __('Failed to copy to clipboard: ') + err,
+						indicator: 'red',
+					});
+				});
 			})
 		}
 	}
