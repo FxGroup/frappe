d546ae0702483f9e3e1df0fe5280421b95095660
Mitch Stokes <mitch@fxmed.co.nz>
2026-02-26 11:50:26 +1300

    System support impersonate

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -1356,7 +1356,7 @@ def get_enabled_users():
 def impersonate(user: str, reason: str = None):
 	# Allow Administrator and users with "Software Developer" role
 	# All the impersonation code doesn't assume anything about user.
-	frappe.only_for(["Administrator", "Software Developer"])
+	frappe.only_for(["Administrator", "Software Developer", "System Support"])
 
 	impersonator = frappe.session.user
 
@@ -1381,21 +1381,13 @@ def impersonate(user: str, reason: str = None):
 		}
 	).insert(ignore_permissions=True, ignore_links=True)
 
-	# notification = frappe.new_doc(
-	# 	"Notification Log",
-	# 	for_user=user,
-	# 	from_user=frappe.session.user,
-	# 	subject=_("{0} just impersonated as you. They gave this reason: {1}").format(impersonator, reason),
-	# )
-	# notification.set("type", "Alert")
-	# notification.insert(ignore_permissions=True)
 	frappe.local.login_manager.impersonate(user)
 
 
 @frappe.whitelist(methods=["POST"])
 def stop_impersonate(user: str, password: str, permission: bool = None):
     if not permission:
-        frappe.only_for(["Administrator", "Software Developer"])
+        frappe.only_for(["Administrator", "Software Developer", "System Support"])
     
     # Verify the user's password
     if not frappe.auth.LoginManager().check_password(user, password):
